de1a7ae2fd808051bfa22364a9b88cf71e998ffb
ivi-dev <videviliyan@gmail.com>
2025-08-10 10:55:42 +0300

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,11 @@
 CONTAINER_NAME=flags-be
 
 docker rm -f $CONTAINER_NAME
-docker run -d --name $CONTAINER_NAME -p 3000:3000 $CONTAINER_NAME:latest
+docker run -d \
+    --name $CONTAINER_NAME \
+    -p 444:444 \
+    --mount type=volume,src=flags-cert-manager-private,dst=/etc/ssl/private/flags \
+    --mount type=volume,src=flags-cert-manager-public,dst=/etc/ssl/certs/flags \
+    $CONTAINER_NAME:latest
 docker network connect flags $CONTAINER_NAME
+docker exec $CONTAINER_NAME nginx
