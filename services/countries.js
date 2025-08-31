75501ba3ffe17e20bea80d11adf388df17588b3d
ivi-dev <videviliyan@gmail.com>
2025-08-31 09:51:01 +0300

    Implement flags shuffling

--- services/countries.js
+++ services/countries.js
@@ -7,12 +7,25 @@ const conn = `mongodb://` +
              `${config['username']}:` +
              `${config['password']}@` +
              `${config['host']}:` +
-             `${config['port']}`;
+             `${config['port']}/?` +
+             `authSource=${config['authSource']}&` +
+             `tls=true&` +
+             `tlsCAFile=/etc/ssl/certs/flags/root.crt`;
 
 const client = new MongoClient(conn);
 
-export async function getCountries() {
+function shuffleArray(array) {
+    for (let i = array.length - 1; i >= 1; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+export async function getCountries(shuffle = true) {
   const db = client.db('flags');
   const countries = db.collection('countries');
-  return await countries.find().toArray();
+  let countries_ = await countries.find().toArray();
+  if (shuffle) countries_ = shuffleArray(countries_);
+  return countries_;
 }
